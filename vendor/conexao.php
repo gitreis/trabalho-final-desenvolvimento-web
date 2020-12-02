3eb3e3765c4673fd2f06700a90e3f22a8194bf54
gitreis <rafael.rcosta.dev@gmail.com>
2020-12-01 21:53:48 -0300

    Alterado a forma de conexao. Criada função para instanciar objeto PDO. Criado página home.php. Criada tela de login. Caso autentique o usuário será direcionado para home.php do contrário permanece na tela de login.

--- vendor/conexao.php
+++ vendor/conexao.php
@@ -1,56 +1,16 @@
 <?php
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASS', '');
+define('DBNAME', 'desweb');
 
-  class Conexao
-  {
-    private const HOST = 'localhost';
-    private const USER = 'root';
-    private const DBNAME = 'desweb';
-    private const PASSWD = '';
-
-    private const OPTIONS = [
-      PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
-      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
-      PDO::ATTR_CASE => PDO::CASE_NATURAL
-    ];
-
-    private static $instance;
-
-    /**
-     * @return PDO
-     */
-    public static function getInstance(): PDO
-    {
-      if(empty(self::$instance)) {
-        try {
-          self::$instance = new PDO(
-            sprintf('
-              dsn: mysql:host=%s;dbname=%s,
-              username:%s,
-              passwd:%s,
-              option:%s'
-              , self::HOST
-              , self::DBNAME
-              , self::USER
-              , self::PASSWD
-              , self::OPTIONS
-            )
-          );
-
-        } catch (PDOException $exception) {
-          die('<h1>Opa! Falha ao conectar!</h1>');
-        }
-      }
-      return self::$instance;
-    }
-
-    final private function __construct()
-    {
-
-    }
-
-    final private function __clone()
-    {
-
-    }
+function fConexao() {
+  try {
+    $conn = new pdo('mysql:host=' . HOST . ';dbname=' . DBNAME, USER, PASS);
+    $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    echo "Conexão com banco de dados realizada com sucesso.";
+  } catch (PDOException $e) {
+    echo "Erro: Conexão com banco de dados não foi realizada com sucesso. Erro gerado " . $e->getMessage();
   }
+  return $conn;
+}
--- vendor/funcoes.php
+++ vendor/funcoes.php
@@ -1,6 +1,25 @@
 <?php
-  require_once 'index.html';
+
+  require_once 'conexao.php';
 
   if(!empty($_POST['usuario']) && !empty($_POST['senha'])) {
+    $oPdo   = fConexao();
+    $sStmt  = $oPdo->prepare('SELECT LOGIN, SENHA FROM USUARIO WHERE LOGIN= :login AND SENHA= :senha');
+    $sStmt->bindParam(':login' ,$_POST['usuario']);
+    $sStmt->bindParam(':senha', $_POST['senha']);
+    $sStmt->execute();
+    $oQuery = $sStmt->fetch(PDO::FETCH_ASSOC);
+
+    if(($oQuery['LOGIN'] == $_POST['usuario']) && ($oQuery['SENHA'] == $_POST['senha'])) {
+      echo 'ok';
+      $_SESSION['logado'] = true;
+      header('Location: ./home.php');
+
+    }else {
+      echo 'não ok';
+      $_SESSION['erro'] = true;
+      header('Location: ../index.html');
+    }
+
 
   }
